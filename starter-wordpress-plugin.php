95ec1e2b9c75d9cd0d0c57fe045ea4592b4a20c9
Justin Greer <justin@justin-greer.com>
2019-12-29 22:43:32 -0500

    Basic Plugin Header

---
+++ starter-wordpress-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Starter WordPress Plugin
+ * Plugin URI: https://github.com/justingreerbbi/Starter-WordPress-Plugin
+ * Description: A starter plugin for WordPress. Get Started Fast!!!
+ * Author: justingreerbbi
+ * Version: 0.1.0
+ * Author URI: https://dash10.digital
+ * Text Domain: starter-plugin
+ */
+
+/**
+ * Shortcut constant to the path of this file.
+ *
+ * This will allow you to hook into this plugin file path from anywhere by simply using the STARTER_PLUGIN_DIR constant.
+ */
+define( 'STARTER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
